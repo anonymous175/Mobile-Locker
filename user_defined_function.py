6b35acf14ad12b694397094788de32b24bc0ef4a
Mr. X <125695538+anonymous175@users.noreply.github.com>
2023-02-19 07:49:50 -0800

    Add files via upload

---
+++ user_defined_function.py
@@ -0,0 +1,15 @@
+print("Anonymous")
+
+print("Unlock your password")
+passw = "umair"
+password = input("Enter Password: ")
+#for(i in range(1,3))
+
+while passw != password:
+    
+      password = input("Enter again :")
+if passw == password:
+          
+ print("UNLOACK !!!!")
+    
+      
